9fa31f894077075b78c9d7edbaab32818dc9d356
Igor Bugaytsov <i.bugaytsov@mail.ru>
2025-03-09 19:47:32 +0300

    выполнение задания 16

--- main.js
+++ main.js
@@ -1,46 +1,33 @@
 'use strict'
 
-const DomElement = function (selector, height, width, bg, fontSize) {
-    this.selector = selector
-    this.height = height
-    this.width = width
-    this.bg = bg
-    this.fontSize = fontSize
-}
+class First {
+    constructor() {
 
-DomElement.prototype.addElement = function () {
-    if (this.selector.startsWith('.')) {
-        const div = document.createElement('div')
-        div.classList = 'block'
-        div.textContent = 'Это блок'
-        div.style.cssText = `
-        width: ${this.width}px;
-        height: ${this.height}px;
-        background-color: ${this.bg};
-        font-size: ${this.fontSize}px;
-        `
-        document.body.append(div)
-        console.log(div);
-    } else if (this.selector.startsWith('#')) {
-        const p = document.createElement('p')
-        p.setAttribute('id', 'paragraph')
-        p.textContent = 'Это параграф'
-        p.style.cssText = `
-        width: ${this.width}px;
-        height: ${this.height}px;
-        background-color: ${this.bg};
-        font-size: ${this.fontSize}px;
-        `
-        document.body.append(p)
-        console.log(p);
+    }
+    hello() {
+        console.log(`Привет, я метод родителя`);
+    }
+
+}
+class Second extends First {
+    constructor() {
+        super()
+    }
+    newHello() {
+        super.hello()
+        console.log(`А я метод дочерний`);
+
     }
 }
 
-const block = new DomElement('.block', 100, 100, 'green', 15)
-const paragraph = new DomElement('#p', 150, 500, 'yellow', 20)
-block.addElement()
-paragraph.addElement()
+const obj = new Second
+obj.newHello()
 
-console.log(block);
-console.log(paragraph);
+
+
+
+
+
+
+
 
