a27f436288b9b8c548e76d5257eac6e4f7c9c8e9
Igor Bugaytsov <i.bugaytsov@mail.ru>
2025-03-09 17:07:45 +0300

    доделал задание 15

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 'use strict'
 
-const DomElement = function () {
+const DomElement = function (selector, height, width, bg, fontSize) {
+    this.selector = selector
+    this.height = height
+    this.width = width
+    this.bg = bg
+    this.fontSize = fontSize
+}
 
-    this.selector = ''
-    this.height = 0
-    this.widht = 0
-    this.bg = 'green'
-    this.fontSize = 24
-
-    this.addElem = function () {
-        if (this.selector.startsWith('.')) {
-            const div = document.createElement('div')
-            div.classList.add('block')
-            div.innerText = 'Текст блока'
-            div.style.cssText = 'height: 200px; width: 200px; font-size: 32px; background-color: green;'
-            document.body.append(div)
-            // div.innerText = prompt('Введите текс блока')
-            console.log(div);
-            console.dir(div);
-        }
-        if (this.selector.startsWith('#')) {
-            const p = document.createElement('p')
-            p.setAttribute('id', 'best')
-            p.innerText = 'Текст параграфа'
-            p.style.cssText = ' font-size: 32px; height: 200px; width: 200px; background-color: blue;'
-            document.body.append(p)
-            // p.innerText = prompt('Введите текст параграфа')
-            console.log(p);
-            console.dir(p);
-        }
-
-
+DomElement.prototype.addElement = function () {
+    if (this.selector.startsWith('.')) {
+        const div = document.createElement('div')
+        div.classList = 'block'
+        div.textContent = 'Это блок'
+        div.style.cssText = `
+        width: ${this.width}px;
+        height: ${this.height}px;
+        background-color: ${this.bg};
+        font-size: ${this.fontSize}px;
+        `
+        document.body.append(div)
+        console.log(div);
+    } else if (this.selector.startsWith('#')) {
+        const p = document.createElement('p')
+        p.setAttribute('id', 'paragraph')
+        p.textContent = 'Это параграф'
+        p.style.cssText = `
+        width: ${this.width}px;
+        height: ${this.height}px;
+        background-color: ${this.bg};
+        font-size: ${this.fontSize}px;
+        `
+        document.body.append(p)
+        console.log(p);
     }
 }
 
-const newObj = new DomElement
+const block = new DomElement('.block', 100, 100, 'green', 15)
+const paragraph = new DomElement('#p', 150, 500, 'yellow', 20)
+block.addElement()
+paragraph.addElement()
 
-newObj.selector = prompt('Введите название', '.block или #block')
-newObj.addElem()
+console.log(block);
+console.log(paragraph);
 
-console.log(newObj);
